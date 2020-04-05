139b21c2f876429d2c25d8e223326b2eb7a1ad78
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2020-04-04 20:41:40 -0400

    Simplify layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Maine COVID-19 Data`,
+    description: `Compilation of data from Maine CDC, US CDC, NYTimes, and Portland Press Herald about COVID-19 in Maine.`,
     author: `@gatsbyjs`,
   },
   plugins: [
@@ -15,20 +15,5 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
   ],
 }
