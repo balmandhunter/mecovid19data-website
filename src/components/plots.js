11d35973ff3a005244ce3d8426a7a1aaaaa48a79
Andy Almand-Hunter <andrewalmandhunter@gmail.com>
2020-04-05 21:42:27 -0400

    Use responsive plots
    The plots come in two sizes, 'small', indented for mobile, and 'large',
    intended for desktop.  Add a window resize listener in the plot
    container, and pass the window width to the plot elements so they can
    choose the best size to request from the server.

--- src/components/plots.js
+++ src/components/plots.js
@@ -1,5 +1,23 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { useStaticQuery, graphql } from "gatsby"
+
+const MOBILE_BREAKPOINT = 640
+
+function useWidth() {
+  const [width, setWidth] = useState(window.innerWidth)
+
+  useEffect(() => {
+    function handleResize() {
+      setWidth(window.innerWidth)
+    }
+    window.addEventListener("resize", handleResize)
+    return () => {
+      window.removeEventListener("resize", handleResize)
+    }
+  }, [])
+
+  return width
+}
 
 const Plots = () => {
   const data = useStaticQuery(
@@ -23,6 +41,8 @@ const Plots = () => {
     `
   )
 
+  const width = useWidth()
+
   return (
     <>
       {data.allMarkdownRemark.edges.map(({ node }) => (
@@ -30,19 +50,27 @@ const Plots = () => {
           key={node.frontmatter.slug}
           filename={node.frontmatter.filename}
           html={node.html}
+          width={width}
         />
       ))}
     </>
   )
 }
 
-const Plot = ({ filename, html }) => (
-  <>
-    <figure>
-      <embed type="image/svg+xml" src={`/plot/${filename}`} />
-    </figure>
-    <div className="plot-content" dangerouslySetInnerHTML={{ __html: html }} />
-  </>
-)
+const Plot = ({ filename, html, width }) => {
+  const size = width < MOBILE_BREAKPOINT ? "small" : "large"
+
+  return (
+    <>
+      <figure>
+        <embed type="image/svg+xml" src={`/plot/${filename}?size=${size}`} />
+      </figure>
+      <div
+        className="plot-content"
+        dangerouslySetInnerHTML={{ __html: html }}
+      />
+    </>
+  )
+}
 
 export default Plots
